ca15ed77f514b02988637f61686d2ade64bd4aae
cnszz <cnszz39@gmail.com>
2021-05-20 00:28:49 +0900

    add note favorite logic

--- lib/pages/note/note_detail_detail.dart
+++ lib/pages/note/note_detail_detail.dart
@@ -7,6 +7,8 @@ import 'package:main_flutter_app/models/note/note.dart';
 class NoteDetailPage extends StatefulWidget {
   final Note currentNote;
   bool isEdit = false;
+  bool isFavorite = false;
+  String noteCollectionByUserId = '';
   final FirebaseFirestore firestore;
 
   NoteDetailPage({this.currentNote, this.firestore});
@@ -15,6 +17,31 @@ class NoteDetailPage extends StatefulWidget {
 }
 
 class NoteDetailPageState extends State<NoteDetailPage> {
+  DocumentReference noteReference;
+
+  DocumentReference userReference;
+
+  @override
+  void initState() {
+    super.initState();
+    noteReference = firestore.doc('note/${widget.currentNote.id}');
+    userReference = firestore.doc('user/7EIgrvyTNLX5RWJtAsI5aF0jXaJ3');
+
+    firestore
+        .collection('note_collection_by_user')
+        .where('note', isEqualTo: noteReference)
+        .where('user', isEqualTo: userReference)
+        .get()
+        .then((value) {
+      if (value.docs.length > 0) {
+        setState(() {
+          widget.isFavorite = true;
+          widget.noteCollectionByUserId = value.docs[0].id;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     TextEditingController noteTitleController = new TextEditingController();
@@ -24,7 +51,37 @@ class NoteDetailPageState extends State<NoteDetailPage> {
 
     return ResponsivePage(
       mobilePage: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: Icon(
+                  widget.isFavorite ? Icons.favorite : Icons.favorite_border),
+              onPressed: () {
+                setState(() {
+                  widget.isFavorite = !widget.isFavorite;
+                });
+
+                if (!widget.isFavorite) {
+                  widget.firestore
+                      .doc(
+                          'note_collection_by_user/${widget.noteCollectionByUserId}')
+                      .delete();
+                  widget.noteCollectionByUserId = '';
+                } else {
+                  widget.firestore
+                      .collection('note_collection_by_user')
+                      .add({'note': noteReference, 'user': userReference}).then(
+                          (value) {
+                    setState(() {
+                      widget.noteCollectionByUserId = value.id;
+                      widget.isFavorite = true;
+                    });
+                  });
+                }
+              },
+            ),
+          ],
+        ),
         floatingActionButton: FloatingActionButton(
           child: Icon(widget.isEdit ? Icons.save : Icons.edit_outlined),
           onPressed: () {
